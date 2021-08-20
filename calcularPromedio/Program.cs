91ea19fc20cd462e46417692f11d1d13647c82b5
Hlb <Hlb@DESKTOP-8Q1HLA1>
2021-08-19 22:28:02 -0500

    Agregar archivos de proyecto.

---
+++ calcularPromedio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcularPromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float primerNota, segundaNota, tereraNota, Promedio;
+
+            Console.Write("Digite su primer nota: ");
+            primerNota = float.Parse(Console.ReadLine());
+            Console.Write("Digite su segunda nota: ");
+            segundaNota = float.Parse(Console.ReadLine());
+            Console.Write("Digite su tercera nota: ");
+            tereraNota = float.Parse(Console.ReadLine());
+
+            Promedio = (primerNota + segundaNota + tereraNota) / 3;
+
+            if(Promedio > 3)
+            {
+                Console.WriteLine("Asignatura aprobada.");
+            } else
+            {
+                Console.WriteLine("Asignatura perdida.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
